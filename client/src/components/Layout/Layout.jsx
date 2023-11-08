19d1422be9ef704778dcbdbdb7ed3ad5fdc1b8cf
brunosarti83 <brunosarti.bs@gmail.com>
2023-11-08 09:48:29 -0300

    feat(navigation back home from Header)

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,9 +1,19 @@
 /* eslint-disable react/prop-types */
+import { useNavigate } from "react-router-dom";
+
 export default function Layout({ children }) {
+  const navigate = useNavigate();
   return (
     <div>
       <div className="bg-gray-800 text-white p-5">
-        <h1 className="text-2xl font-bold cursor-pointer">TechBlog - MERN</h1>
+        <h1
+          className="text-2xl font-bold cursor-pointer"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          TechBlog - MERN
+        </h1>
       </div>
       <div className="p-5">{children}</div>
     </div>
